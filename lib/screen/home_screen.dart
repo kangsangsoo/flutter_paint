cdd42775ba407bad67ff539e3a093db8db593535
kangsangsoo <rkdtkdtn0706@korea.ac.kr>
2022-03-03 01:36:20 +0900

    eraser test

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -7,6 +7,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int mode = -1;
+
   @override
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
@@ -40,13 +42,73 @@ class _HomeScreenState extends State<HomeScreen> {
                   child: Container(
                     // height: 1000, // 최댓값을 주면은 Appbar를 벗어나지 못하나?
                     color: Colors.blueGrey,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        InkWell(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'pen and eraser mode',
+                                style: TextStyle(
+                                    color: mode == 0
+                                        ? Colors.white
+                                        : Colors.black),
+                              )
+                            ],
+                          ),
+                          onTap: () {
+                            setState(() {
+                              if (mode == 0)
+                                mode = -1;
+                              else
+                                mode = 0;
+                            });
+                          },
+                        ),
+                        InkWell(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [Text('block mode')],
+                          ),
+                          onTap: () {},
+                        ),
+                        InkWell(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [Text('capture mode')],
+                          ),
+                          onTap: () {},
+                        ),
+                      ],
+                    ),
                   ),
                 ),
                 Flexible(
                   flex: 3,
                   child: Container(
                     // height: 1000, // 최댓값을 주면은 Appbar를 벗어나지 못하나?
-                    color: Colors.white60,
+                    color: Colors.red,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        InkWell(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [Text('pen')],
+                          ),
+                          onTap: () {},
+                        ),
+                        InkWell(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [Text('eraser')],
+                          ),
+                          onTap: () {},
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ],
