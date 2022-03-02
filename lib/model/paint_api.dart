797dd173f0583aefd3f2018397e732f9f9cbea35
kangsangsoo <rkdtkdtn0706@korea.ac.kr>
2022-03-02 22:13:48 +0900

    문제 1. 스트림빌더 vs setState 2. 마우스에서 떼야 결과가 보여짐

--- lib/model/paint_api.dart
+++ lib/model/paint_api.dart
@@ -1,67 +1,84 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+class Line{
+  Offset offset;
+  Line(this.offset);
+}
+
 class PaintApi extends StatefulWidget {
   @override
   _PaintApiState createState() => _PaintApiState();
 }
 
 class _PaintApiState extends State<PaintApi> {
+  List<Line> lines = <Line>[];
+  List<List<Line>> linesSet = <List<Line>> [];
+
   @override
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
     double width = screenSize.width;
     double height = screenSize.height;
     return GestureDetector(
-      onPanStart: (_) {
-        print("onPanStart");
-      },
-      onPanEnd: (_) {
-        print("onPanEnd");
-      },
-      onPanUpdate: (_) {
-        print("onPanUpdate");
-      },
+      onPanStart: onPanStart,
+      onPanEnd: onPanEnd,
+      onPanUpdate: onPanUpdate,
       child: Container(
         width: width,
         height: height - 30, // 다른 방법을.. 생각해보자
         color: Colors.yellow[100],
         child: CustomPaint(
-          painter: Test(),
+          painter: Test(linesSet),
         ),
       ),
     );
   }
+
+  void onPanStart(DragStartDetails details) {
+    setState(() {
+      lines = <Line>[];
+    });
+    // 라인 시작
+  }
+  void onPanUpdate(DragUpdateDetails details) {
+    setState(() {
+      // 현재 좌표를 가져와야 한다.
+      RenderBox renderBox = context.findRenderObject() as RenderBox;
+      Offset offset = renderBox.globalToLocal(details.globalPosition);
+      // 라인 추가
+      lines.add(Line(offset));
+    });
+  }
+  void onPanEnd(DragEndDetails details) {
+    setState(() {
+      // 라인 종료
+      linesSet.add(lines);
+      print('end');
+    });
+  }
 }
 
+
 class Test extends CustomPainter {
+  List<List<Line>> linesSet;
+  Test(this.linesSet);
+
+
   @override
   void paint(Canvas canvas, Size size) {
     Paint paintMountains = Paint()
-      ..style = PaintingStyle.fill
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 5.0
       ..color = Colors.brown;
-    // 2
-    Paint paintSun = Paint()
-      ..style = PaintingStyle.fill
-      ..color = Colors.deepOrangeAccent;
 
-    // 3
-    Path path = Path();
-    path.moveTo(0, 250);
-    path.lineTo(100, 200);
-    path.lineTo(150, 150);
-    path.lineTo(200, 50);
-    path.lineTo(250, 150);
-    path.lineTo(300, 200);
-    path.lineTo(size.width, 250);
-    path.lineTo(0, 250);
-    canvas.drawPath(path, paintMountains);
-
-    // 4
-    path = Path();
-    path.moveTo(100, 100);
-    path.addOval(Rect.fromCircle(center: Offset(100, 100), radius: 25));
-    canvas.drawPath(path, paintSun);
+    if(linesSet.length == 0) return;
+    for (var j = 0; j < linesSet.length; j++) {
+      for (var i = 0; i < linesSet[j].length - 1; i++) {
+        canvas.drawLine(
+            linesSet[j][i].offset, linesSet[j][i + 1].offset, paintMountains);
+      }
+    }
   }
 
   @override
