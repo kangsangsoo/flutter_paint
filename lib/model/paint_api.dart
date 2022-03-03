a3ee2aa6311e4215aea4adcb5071d23390cd319f
kang <rkdtkdtn0706@korea.ac.kr>
2022-03-03 20:57:55 +0900

    type error test, test1

--- lib/model/paint_api.dart
+++ lib/model/paint_api.dart
@@ -27,25 +27,45 @@ class _PaintApiState extends State<PaintApi> {
     Size screenSize = MediaQuery.of(context).size;
     double width = screenSize.width;
     double height = screenSize.height;
-    return GestureDetector(
-      onPanStart: onPanStart,
-      onPanEnd: onPanEnd,
-      onPanUpdate: onPanUpdate,
-      child: Container(
-        width: width,
-        height: height - 30, // 다른 방법을.. 생각해보자
-        color: Colors.yellow[100],
-        child: RepaintBoundary(
-          child: StreamBuilder<Line>(
-            stream: linesStreamController.stream,
-            builder: (context, snapshot) {
-              return CustomPaint(
-                painter: Test(lines),
-              );
-            },
+
+    return Stack(
+      children: [
+        Container(
+          width: width,
+          height: height - 30, // 다른 방법을.. 생각해보자
+          color: Colors.yellow[100],
+          child: RepaintBoundary(
+            child: StreamBuilder<List<Line>>(
+              stream: linesSetStreamController.stream,
+              builder: (context, snapshot) {
+                return CustomPaint(
+                  painter: Test1(linesSet),
+                );
+              },
+            ),
           ),
         ),
-      ),
+        GestureDetector(
+          onPanStart: onPanStart,
+          onPanEnd: onPanEnd,
+          onPanUpdate: onPanUpdate,
+          child: Container(
+            width: width,
+            height: height - 30, // 다른 방법을.. 생각해보자
+            color: Colors.transparent,
+            child: RepaintBoundary(
+              child: StreamBuilder<Line>(
+                stream: linesStreamController.stream,
+                builder: (context, snapshot) {
+                  return CustomPaint(
+                    painter: Test(lines),
+                  );
+                },
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 
@@ -65,8 +85,8 @@ class _PaintApiState extends State<PaintApi> {
   void onPanEnd(DragEndDetails details) {
     // 라인 종료
     linesSet.add(lines);
+    print(linesSet.length);
     linesSetStreamController.add(lines);
-    print('end');
   }
 }
 
@@ -83,9 +103,9 @@ class Test extends CustomPainter {
       ..color = Colors.brown;
 
     if (linesSet.length == 0) return;
-    for(var i = 0; i < linesSet.length - 1; i++) {
+    for (var i = 0; i < linesSet.length - 1; i++) {
       canvas.drawLine(
-                  linesSet[i].offset, linesSet[i + 1].offset, paintMountains);
+          linesSet[i].offset, linesSet[i + 1].offset, paintMountains);
     }
     // for (var j = 0; j < linesSet.length; j++) {
     //   for (var i = 0; i < linesSet[j].length - 1; i++) {
@@ -100,3 +120,39 @@ class Test extends CustomPainter {
     return true;
   }
 }
+
+
+class Test1 extends CustomPainter {
+  List<List<Line>> linesSet;
+
+  Test1(this.linesSet);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paintMountains = Paint()
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 5.0
+      ..color = Colors.brown;
+
+    if (linesSet.length == 0) return;
+
+    // for (var i = 0; i < linesSet.length - 1; i++) {
+    //   canvas.drawLine(
+    //       linesSet[i].offset, linesSet[i + 1].offset, paintMountains);
+    // }
+    print('herere');
+    for (var j = 0; j < linesSet.length; j++) {
+      for (var i = 0; i < linesSet[j].length - 1; i++) {
+        canvas.drawLine(
+            linesSet[j][i].offset, linesSet[j][i + 1].offset, paintMountains);
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(Test delegate) {
+    return true;
+  }
+}
+
+// 캡쳐하는 방법은 지정된 범위 내에서 모든 유효한 line들만 가져오도록
